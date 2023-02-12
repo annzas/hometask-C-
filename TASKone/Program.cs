df1f4186547a4cb3aa5c74d57ad1a2ad4fe4af04
zaslove <zasloveanna@gmail.com>
2023-02-12 15:52:53 +0200

    first task

---
+++ TASKone/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int firstN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввидите третье число: ");
+int thirdN = Convert.ToInt32(Console.ReadLine());
+
+int max = firstN;
+
+if (secondN>max)
+{
+    max=secondN;
+}
+if (thirdN>max)
+{
+    max=thirdN;
+}
+Console.WriteLine (max);
